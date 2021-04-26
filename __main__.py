32eb1f591ad2df578beef57a047e42c87c64b956
spookybear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-26 11:24:59 -0700

    more pep8 and some changes for figment-chan

--- __main__.py
+++ __main__.py
@@ -2,11 +2,13 @@ import pypresence
 import requests
 import gd
 import os
+
 try:
-    percent = float(input("Enter the percent you want to deafen at:\n"))
+    deaf_percent = float(input("Enter the percent you want to deafen at:\n"))
 except ValueError:
     print("Please enter a number!")
     os.exit(1)
+
 
 def exchange_code(code: int) -> str:
     data = {
@@ -15,7 +17,10 @@ def exchange_code(code: int) -> str:
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    r = requests.post("https://zbot.figmentcoding.me/get_token", data=data, headers=headers) # Proxy server to not leak client secret
+    # Proxy server to not leak client secret
+    r = requests.post("https://zbot.figmentcoding.me/get_token",
+                      data=data,
+                      headers=headers)
     r.raise_for_status()
 
     if (r.status_code != 200 or r.text == "-1"):
@@ -44,31 +49,26 @@ client.authenticate(code)
 
 deafened = False
 
-def deafen(deaf=True): # shorthand for client.set_voice_settings
+
+def deafen(deaf=True):  # shorthand for client.set_voice_settings
     deafened = deaf
-     
     client.set_voice_settings(deaf=deaf)
 
 
 deafen(False)
 os.system("cls")
 
-try:
-    while True:
-        try:
-            memory = gd.memory.get_memory()
-        except RuntimeError:
-            print("Geometry Dash was closed")
-            os.exit(1)
+while True:
+    try:
+        memory = gd.memory.get_memory()
+    except RuntimeError:
+        print("Geometry Dash was closed")
+        os.exit(1)
 
+    percent = memory.get_percent()
+    dead = memory.is_dead()
 
-        if ((memory.get_percent() >= 100 or
-            memory.get_percent() < percent or
-            memory.is_dead()) and deafened):
+    if (percent >= 100 or percent < pdeaf_percent or dead) and deafened:
             deafen(False)
-        elif ((memory.get_percent() >= percent
-             and memory.get_percent() < 100)
-             and not deafened):
-            deafen(True)
- except KeyboardInterrupt:
-    pass
+    elif (percent >= deaf_percent and percent < 100) and not deafened:
+        deafen(True)
