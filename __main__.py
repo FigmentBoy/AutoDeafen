fb4ac3e4bc551355dc0e7557b649f601faf9bc19
spookybear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-26 11:12:42 -0700

    pep8

--- __main__.py
+++ __main__.py
@@ -1,18 +1,21 @@
-import pypresence, requests, gd, os
+import pypresence
+import requests
+import gd
+import os
 try:
     percent = float(input("Enter the percent you want to deafen at:\n"))
 except ValueError:
     print("Please enter a number!")
-    exit(1)
+    os.exit(1)
 
-def exchange_code(code):
+def exchange_code(code: int) -> str:
     data = {
         'code': code
     }
     headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded"
     }
-    r = requests.post('https://zbot.figmentcoding.me/get_token', data=data, headers=headers) # Proxy server to not leak client secret
+    r = requests.post("https://zbot.figmentcoding.me/get_token", data=data, headers=headers) # Proxy server to not leak client secret
     r.raise_for_status()
 
     if (r.status_code != 200 or r.text == "-1"):
@@ -25,45 +28,47 @@ try:
     memory = gd.memory.get_memory()
 except RuntimeError:
     print("Geometry Dash is closed")
-    exit(1)
+    os.exit(1)
 
-c = pypresence.Client(836272609834434561)
-c.start()
+client = pypresence.Client(836272609834434561)
+client.start()
 
-auth = c.authorize(836272609834434561, ["rpc"])
+auth = client.authorize(836272609834434561, ["rpc"])
 code = exchange_code(auth['data']['code'])
 
 if (code == -1):
     print("Internal Server Error")
-    exit(1)
+    os.exit(1)
 
-c.authenticate(code)
+client.authenticate(code)
 
-def deafen():
-    c.set_voice_settings(deaf=True)
+deafened = False
 
-def undeafen():
-    c.set_voice_settings(deaf=False)
-
-
-undeafen()
-deafened = False;
-os.system('cls');
-
-while True:
-    try:
-        memory = gd.memory.get_memory()
-    except RuntimeError:
-        print("Geometry Dash was closed")
-        exit(1)
-    
-    
-    if (memory.get_percent() >= 100 or memory.get_percent() < percent or memory.is_dead()) and deafened:
-        undeafen()
-        deafened = False
-    elif (memory.get_percent() >= percent and memory.get_percent() < 100) and not deafened:
-        deafen()
-        deafened = True
+def deafen(deaf=True): # shorthand for client.set_voice_settings
+    deafened = deaf
+     
+    client.set_voice_settings(deaf=deaf)
 
 
+deafen(False)
+os.system("cls")
 
+try:
+    while True:
+        try:
+            memory = gd.memory.get_memory()
+        except RuntimeError:
+            print("Geometry Dash was closed")
+            os.exit(1)
+
+
+        if ((memory.get_percent() >= 100 or
+            memory.get_percent() < percent or
+            memory.is_dead()) and deafened):
+            deafen(False)
+        elif ((memory.get_percent() >= percent
+             and memory.get_percent() < 100)
+             and not deafened):
+            deafen(True)
+ except KeyboardInterrupt:
+    pass
